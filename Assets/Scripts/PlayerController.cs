37b9537b4c0fca55240a2931addc043813c35f3d
Baxi19 <randald1991@gmail.com>
2021-04-14 20:02:47 -0600

    :octocat: Player added and animations to walk and jump :fire:, also validate to jump only one time :alien:

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    float moveSpeed = 6f;
-
+    [SerializeField] Rigidbody playerRb;
+    [SerializeField] float moveSpeed = 6f;
+    [SerializeField] float jumpSpeed = 400f;
+    [SerializeField] Animator playerAnimator;
     Vector3 forward, right;
+    bool isGrounded = true;
+
+    
     void Start()
     {
         forward = Camera.main.transform.forward;
@@ -18,6 +22,26 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if(Input.GetAxis("HorizontalKey") == 0 && Input.GetAxis("VerticalKey") == 0){
+            playerAnimator.SetBool("isWalking", false);
+        }else if(Input.GetAxis("HorizontalKey") < 0 ){
+            playerAnimator.SetBool("isWalking", true);
+        }else if(Input.GetAxis("HorizontalKey") > 0 ){
+            playerAnimator.SetBool("isWalking", true);
+        }else if(Input.GetAxis("VerticalKey") < 0 ){
+            playerAnimator.SetBool("isWalking", true);
+        }else if(Input.GetAxis("VerticalKey") > 0 ){
+            playerAnimator.SetBool("isWalking", true);
+        }
+        
+        if(isGrounded){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                playerRb.AddForce(Vector3.up * jumpSpeed);
+                isGrounded = false;
+                playerAnimator.SetTrigger("isJump");
+            }
+        }
+        
         if(Input.anyKey){
             move();
         }
@@ -33,5 +57,11 @@ public class PlayerController : MonoBehaviour
         transform.position += rightMovement;
         transform.position += upMovement;
 
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Ground")){
+            isGrounded = true;
+        }
     }
 }
