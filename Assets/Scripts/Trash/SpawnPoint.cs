ca0d5d545b24d41de44a8dbbe31e7c83ed295046
Kevin Solano <ksolanoj@icloud.com>
2021-04-22 21:20:10 -0600

    Some changes

--- Assets/Scripts/Trash/SpawnPoint.cs
+++ Assets/Scripts/Trash/SpawnPoint.cs
@@ -14,18 +14,21 @@ public class SpawnPoint : MonoBehaviour
     [Header("Number of Objects")]
     public int numberOfObjects = 5;
 
+    [Header("Spawn Area Number")]
+    public int number = 5;
+
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnTrash", 0f, seconds);
+        InvokeRepeating("SpawnTrash", 5f, seconds);
     }
 
     public void SpawnTrash()
     {
         for (int i = 0; i < numberOfObjects; i++)
         {
-            float xValue = Random.Range(-3, 3);
-            float zValue = Random.Range(-3, 3);
+            float xValue = Random.Range(-number, number);
+            float zValue = Random.Range(-number, number);
             var gameObjectSelected = trashObjects[Random.Range(0, trashObjects.Length)];
             Instantiate(gameObjectSelected, transform.position + new Vector3(xValue, 0, zValue), Quaternion.identity);
         }
