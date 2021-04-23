79a807bd06751f6e576532a0f7328046dca74c42
Baxi19 <randald1991@gmail.com>
2021-04-22 22:14:42 -0600

    Agents can move :alien: also a police to ask the game info :octocat:

---
+++ Assets/Scripts/Agents/RandomSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSpanner : MonoBehaviour
+{
+    [SerializeField] GameObject[] spawnees;
+    [SerializeField] Transform spawnPos;
+
+    int randomInt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            SpawnRandom();
+        }
+    }
+
+    void SpawnRandom(){
+        randomInt = Random.Range(0, spawnees.Length);
+        Instantiate(spawnees[randomInt], spawnPos.position, spawnPos.rotation);
+    }
+}
