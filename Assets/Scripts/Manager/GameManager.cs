3b43b6bc49d6ac69eb5d1637f92d48262009023b
LizanoLeo123 <leoln123@live.com>
2021-04-30 21:12:10 -0600

    Added win condition and dialogues logic

---
+++ Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Next Scene")]
+    public string nextScene;
+
+    [Header("Inventory")]
+    public InventoryObject inventory;
+
+    private UI_Manager uiManager;
+
+    private void Start()
+    {
+        uiManager = GameObject.Find("Canvas").GetComponent<UI_Manager>();
+        inventory.money = 0;
+    }
+
+    public void CheckCondition()
+    {
+        if (inventory.money >= 2700) //2700
+            StartCoroutine(LoadNextScene());
+    }
+
+    IEnumerator LoadNextScene()
+    {
+        //foreach (InventorySlot inventoryS in inventory.Container)
+        //{
+        //    inventory.removeItem(inventoryS.item);
+        //}
+        int size = inventory.Container.Count;
+        while(size > 0)
+        {
+            inventory.removeItem(inventory.Container[0].item);
+            size--;
+        }
+        inventory.totalAmount = 0;
+        uiManager.ShowFinalMessage();
+        yield return new WaitForSeconds(3.5f);
+        uiManager.FadeOut();
+        yield return new WaitForSeconds(0.9f);
+        SceneManager.LoadScene(nextScene);   
+    }
+}
